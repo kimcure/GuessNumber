0d9eca3dd8626f93bf088e12f7e454f45760773f
kimcure <dellwons@gmail.com>
2025-04-29 11:44:04 +0900

    25.4.29//nomal

--- main.py
+++ main.py
@@ -39,6 +39,33 @@ while True:
             elif level == "2":
                 print("보통 난이도를 시작합니다.")
 
+                nomalnum = random.randrange(1, 101)
+                nomaltrynum = 0
+
+                while True:
+                    try:
+                        answer = int(input("숫자를 입력해주세요."))
+
+                        if answer < nomalnum:
+                            print("입력한 숫자가 정답보다 작습니다.")
+                            print("시도 횟수:", nomaltrynum)
+                            nomaltrynum += 1
+                        elif answer > nomalnum:
+                            print("입력한 숫자가 정답보다 큽니다.")
+                            print("시도 횟수:", nomaltrynum)
+                            nomaltrynum += 1
+                        elif answer == nomalnum:
+                            print("정답입니다! 프로그램을 종료합니다.")
+                            print("시도 횟수:", nomaltrynum)
+                            quit()
+
+                    except ValueError:
+                        print("올바른 값을 입력해주세요.")
+                    
+                    if nomaltrynum == 10:
+                        print("시도 횟수를 모두 소진했습니다. 프로그램을 종료합니다.")
+                        quit()
+
             elif level == "3":
                 print("어려움 난이도를 시작합니다.")
 
