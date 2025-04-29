8f2f2b1157633a97835f5008e2361d15335261f5
kimcure <dellwons@gmail.com>
2025-04-29 12:02:25 +0900

    25.4.29//hard

--- main.py
+++ main.py
@@ -5,12 +5,12 @@ while True:
     
     if start == "네":
         while True:
-            level = input("난이도를 결정해주세요. \n1. 쉬움(1 ~ 50, 횟수 제한 20) \n2. 보통(1 ~ 100, 횟수 제한 10) \n3. 어려움(1 ~ 1000, 횟수 제한 15)\n")
+            level = input("난이도를 결정해주세요. \n1. 쉬움(1 ~ 50, 횟수 제한 20) \n2. 보통(1 ~ 100, 횟수 제한 10) \n3. 어려움(1 ~ 1000, 횟수 제한 10)\n")
             if level == "1":
                 print("쉬움 난이도를 시작합니다")
 
                 easynum = random.randrange(1, 51)
-                easytrynum = 0
+                easytrynum = 1
 
                 while True:
                     try:
@@ -32,7 +32,7 @@ while True:
                     except ValueError:
                         print("올바른 값을 입력해주세요.")
                     
-                    if easytrynum == 20:
+                    if easytrynum == 21:
                         print("시도 횟수를 모두 소진했습니다. 프로그램을 종료합니다.")
                         quit()
 
@@ -40,7 +40,7 @@ while True:
                 print("보통 난이도를 시작합니다.")
 
                 nomalnum = random.randrange(1, 101)
-                nomaltrynum = 0
+                nomaltrynum = 1
 
                 while True:
                     try:
@@ -62,12 +62,40 @@ while True:
                     except ValueError:
                         print("올바른 값을 입력해주세요.")
                     
-                    if nomaltrynum == 10:
+                    if nomaltrynum == 11:
                         print("시도 횟수를 모두 소진했습니다. 프로그램을 종료합니다.")
                         quit()
 
             elif level == "3":
                 print("어려움 난이도를 시작합니다.")
+
+                hardnum = random.randrange(1, 1001)
+                hardtrynum = 1
+
+                while True:
+                    try:
+                        answer = int(input("숫자를 입력해주세요."))
+
+                        if answer < hardnum:
+                            print("입력한 숫자가 정답보다 작습니다.")
+                            print("시도 횟수:", hardtrynum)
+                            hardtrynum += 1
+                        elif answer > hardnum:
+                            print("입력한 숫자가 정답보다 큽니다.")
+                            print("시도 횟수:", hardtrynum)
+                            hardtrynum += 1
+                        elif answer == hardnum:
+                            print("정답입니다! 프로그램을 종료합니다.")
+                            print("시도 횟수:", hardtrynum)
+                            quit()
+
+                    except ValueError:
+                        print("올바른 값을 입력해주세요.")
+                    
+                    if hardtrynum == 11:
+                        print("시도 횟수를 모두 소진했습니다. 프로그램을 종료합니다.")
+                        quit()
+                
 
     elif start == "아니오":
         print("프로그램을 종료합니다.")
